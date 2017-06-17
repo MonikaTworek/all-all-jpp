2ab986407252df39884e720d2b81d3a974f98906
Przemysław Kobylański <przemko@mac.com>
2017-06-17 12:33:09 +0200

    poprawki

--- prolog/kwadrat.pl
+++ prolog/kwadrat.pl
@@ -7,21 +7,20 @@ main(Xs) :-
 		 [50,42,37,35,33,29,27,25,24,19,18,17,16,15,11,9,8,7,6,4,2],
 		 Xs).
 
-kwadraty(D, Sqs, Xs) :-
-        length(Sqs, N),
+kwadraty(D, Ds, Xs) :-
+        length(Ds, N),
         D1 is D-1,
         length(Xs, N),
         Xs ins 0..D1,
-        length(Ys, N),
-        Ys ins 1..D,
-        zadania(Xs, Ys, Sqs, 1, T1),
-        cumulative(T1, [limit(D)]),
+	length(Ys, N),
+	Ys ins 1..D,
+        zadania(Xs, Ys, Ds, Zadania),
+        cumulative(Zadania, [limit(D)]),
 	% SICStus Prolog z opcją global(true) w cumulative/2 8500ms
         labeling([ffc], Xs).
 
+zadania([], [], [], []).
+zadania([X | L1], [Y | L2], [D | L3], [task(X, D, _, D, _) | L4]) :-
+	Y #= X + D,
+        zadania(L1, L2, L3, L4).
 
-zadania([], [], [], _, []).
-zadania([X | L1], [Y | L2], [D | L3], T, [task(X, D, Y, D, T) | L4]) :-
-        Y #= X + D,
-        T1 is T + 1,
-        zadania(L1, L2, L3, T1, L4).
